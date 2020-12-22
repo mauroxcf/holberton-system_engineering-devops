ff38e0fa5783cb930e578a5b5c88860b1c6d394e
mauroxcf <maurox03@gmail.com>
2020-12-21 21:40:21 -0500

    automate of creating a custom HTTP header response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #creating a custom HTTP header response, but with Puppet.
 exec { 'custom_header':
   command  => 'sudo apt-get update -y;
@@ -6,5 +7,5 @@ exec { 'custom_header':
   sudo ufw allow 'Nginx HTTP';
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   sudo service nginx restart',
-  provider => 'shell'
+  provider => 'shell',
 }
