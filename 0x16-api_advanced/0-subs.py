71195ed756e63285e7b9ea77777c5008daac3473
mauroxcf <maurox03@gmail.com>
2021-02-04 15:59:12 -0500

    function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,10 +15,10 @@ def number_of_subscribers(subreddit):
     data key of the url.
     """
     if subreddit is None:
-        return 0
+        return (0)
     else:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {'user-agent': 'mauroxdev03'}
-        response = reqs.get(url, headers=headers).json()
+        response = reqs.get(url, headers=headers, allow_redirect=False).json()
         subs_count = (response["data"]["subscribers"])
         return subs_count
